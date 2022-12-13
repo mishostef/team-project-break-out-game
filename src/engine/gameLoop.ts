b963f9e47e24d81350fb21328ec1ebb12406d123
mihail.stefanov <mihail.stefanov@amusnet.com>
2022-12-13 15:26:27 +0200

    added logic for removing bricks

--- src/engine/gameLoop.ts
+++ src/engine/gameLoop.ts
@@ -5,6 +5,7 @@ import { Ball } from "../figures/Ball";
 import { Vector } from "../Geometry/Vector";
 import { move } from "./move";
 import { BOARD_HEIGHT, BOARD_WIDTH, BRICK_HEIGHT, BRICK_WIDTH, INITIAL_BALL_X, INITIAL_BALL_Y } from "../utils/constants";
+import { Brick } from "../figures/Brick";
 
 
 const canvasView = new CanvasView('gameCanvas');
@@ -12,15 +13,15 @@ let lastTime = 0;
 let elapsed = 0;
 const STEP_SIZE = 20;
 
-const bricks = createBricks();
+const bricks = createBricks() as null | Brick[];
 const boardImg = document.getElementById('board') as HTMLImageElement;
 const boardPosition = new Vector(canvasView.canvas.width / 2, canvasView.canvas.height - 100);
 const board = new Board(boardPosition, boardImg);
-const ball = new Ball({ x: 200, y: 200 }, "/assets/ball.png");
+const ball = new Ball({ x: INITIAL_BALL_X, y: INITIAL_BALL_Y }, "/assets/ball.png");
 const BALL_DIAMETER = 50;
 const input: { [code: string]: boolean } = {};
 const BRICKS_END = 170;
-
+let index = -1;
 
 window.addEventListener('keydown', event => {
     input[event.code] = true;
@@ -29,10 +30,16 @@ window.addEventListener('keydown', event => {
 window.addEventListener('keyup', event => {
     input[event.code] = false;
 });
+
 export function update(time: number) {
     const delta = time - lastTime;
     lastTime = time;
     elapsed += delta;
+    if (index != -1) {
+        bricks.splice(index, 1);
+        index = -1;
+        ballVelocity.y = -ballVelocity.y;
+    }
     if (elapsed > STEP_SIZE * 5) {
         elapsed = STEP_SIZE * 5;
     }
@@ -57,6 +64,7 @@ export function loop() {
         move(board, boardVelocity);
     }
     canvasView.getContext().clearRect(0, 0, canvasView.canvas.width, canvasView.canvas.height);
+
     canvasView.drawBricks(bricks);
     canvasView.drawBoard(board);
     canvasView.drawBall(ball);
@@ -70,8 +78,9 @@ function collisionDetector() {
     if (isBallCollidingWithBoard()) {
         ballVelocity.y = -ballVelocity.y;
     }
-    if (isBallHittingABrick()) {
-        removeHitBrick();
+    if (isBallNearBricks()) {
+        setHitBrickIndex();
+
     }
     if (ball.position.y >= canvasView.canvas.height - BALL_DIAMETER) {///
         ballVelocity.y = -ballVelocity.y;
@@ -84,24 +93,20 @@ function collisionDetector() {
     }
 }
 
-function removeHitBrick() {
-    const index = bricks.findIndex(b => ((b.position.y - BRICK_HEIGHT / 2 <= ball.position.y)
+function setHitBrickIndex() {
+    index = bricks.findIndex(b => ((b.position.y - BRICK_HEIGHT / 2 <= ball.position.y)
         && (b.position.y + BRICK_HEIGHT / 2 >= ball.position.y)
         && (b.position.x + BRICK_WIDTH / 2 >= ball.position.x)
         && (b.position.x - BRICK_WIDTH / 2 >= ball.position.x)));
-    if (index != -1) {
-        bricks.splice(index, 1);
-    }
-    ballVelocity.x = Math.abs(ballVelocity.x);
 }
 
 function isBallCollidingWithBoard() {
-    return (ball.position.y <= board.position.y - BOARD_HEIGHT / 2 - BALL_DIAMETER / 2
-        && ball.position.y > board.position.y - BOARD_HEIGHT / 2 - BALL_DIAMETER
+    return (ball.position.y <= board.position.y + BOARD_HEIGHT / 2 - BALL_DIAMETER / 2
+        && ball.position.y >= board.position.y - BOARD_HEIGHT / 2 - BALL_DIAMETER / 2
         && ball.position.x <= board.position.x + BOARD_WIDTH / 2
         && ball.position.x >= board.position.x - BOARD_WIDTH / 2)
 }
 
-function isBallHittingABrick() {
+function isBallNearBricks() {
     return (ball.position.y < BRICKS_END)
 }
